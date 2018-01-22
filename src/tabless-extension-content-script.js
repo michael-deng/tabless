076dd98c3fb29d3d85d2c8be363d6db9fc25a560
Michael Deng <michaeldeng18@gmail.com>
2018-01-21 18:03:39 -0800

    Format indents and set initial version

--- src/tabless-extension-content-script.js
+++ src/tabless-extension-content-script.js
@@ -1,28 +1,28 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
-  // Receive request to toggle modal
-  if (msg.text === "toggle") {
+    // Receive request to toggle modal
+    if (msg.text === "toggle") {
 
-    // Respond that content script is running
-    sendResponse({text: "ok"});
+        // Respond that content script is running
+        sendResponse({text: "ok"});
 
-    var modal = document.getElementById("tabless-extension-modal"); // Get the modal, it might not exist yet
+        var modal = document.getElementById("tabless-extension-modal"); // Get the modal, it might not exist yet
 
-    // modal exists
-    if (modal) {
-      $(modal).fadeOut(300, function() {
-        $(this).remove();
-      });
+        // modal exists
+        if (modal) {
+            $(modal).fadeOut(300, function() {
+                $(this).remove();
+            });
+        }
+        // modal doesn't exist yet
+        else {
+            modal = document.createElement('div');
+            modal.id = "tabless-extension-modal";
+            var iframe = document.createElement('iframe');
+            iframe.src = chrome.runtime.getURL("modal.html");
+            iframe.frameBorder = 0;
+            $(modal).append(iframe);
+            $(modal).hide().appendTo("body").fadeIn(300);
+        }
     }
-    // modal doesn't exist yet
-    else {
-      modal = document.createElement('div');
-      modal.id = "tabless-extension-modal";
-      var iframe = document.createElement('iframe');
-      iframe.src = chrome.runtime.getURL("modal.html");
-      iframe.frameBorder = 0;
-      $(modal).append(iframe);
-      $(modal).hide().appendTo("body").fadeIn(300);
-    }
-  }
 });

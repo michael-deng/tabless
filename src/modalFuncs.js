e6894c484d40b8117df64f198f4885c22609d14b
Michael Deng <michaeldeng18@gmail.com>
2018-08-12 16:41:50 -0700

    Make timestamp in main tabs table dynamic

--- src/modalFuncs.js
+++ src/modalFuncs.js
@@ -34,7 +34,7 @@ function addTabRow(key, table) {
     }
 
     // Set the tab title
-    cell2.innerHTML = "<div class=\"tab-title\">" + tab.title + "</div><div class=\"tab-time-elapsed\"></div><div class=\"tab-timer\"></div>";
+    cell2.innerHTML = "<div class=\"tab-title\">" + tab.title + "</div><div class=\"tab-time-elapsed\"></div><div class=\"dot\"> &middot </div><div class=\"tab-timer\"></div>";
 
     // Set up the redirect link on the title
     var title = cell2.getElementsByTagName("div")[0];
@@ -44,23 +44,7 @@ function addTabRow(key, table) {
 
     // Set the time elapsed
     var timeElapsed = cell2.getElementsByClassName("tab-time-elapsed")[0];
-    var distance = Date.now() - created;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    if (days == 0 && hours == 0 && minutes == 0) {
-        timeElapsed.innerHTML = "<1m ago &middot ";
-    } else if (days == 0 && hours == 0) {
-        timeElapsed.innerHTML = minutes + "m ago &middot ";
-    } else if (days == 0) {
-        timeElapsed.innerHTML = hours + "h ago &middot ";
-    } else {
-        timeElapsed.innerHTML = days + "d ago &middot ";
-    }
+    countup(created, timeElapsed);
 
     // Set the timer
     var timer = cell2.getElementsByClassName("tab-timer")[0];
